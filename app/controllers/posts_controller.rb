eec8e96833e723b386634167e3f65d234f833093
Storm <storm.sarracino01@gmail.com>
2022-05-08 14:44:48 -0500

    commit tree dad63e9633aa623bcc091c81ff0b35c5a752307f

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,12 @@ class PostsController < ApplicationController
 
   def index
     posts = Post.all
-    render json: posts
+    render json: posts, each_serializer: PostSerializer
   end
 
   def show
     post = Post.find(params[:id])
-    render json: post
+    render json: post, each_serializer: PostSerializer
   end
 
   private
